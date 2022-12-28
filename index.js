ab705ae68ef7ce2dd3db17ab1856d52b872bf5ff
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2022-12-29 00:09:22 +0800

    Padded hour

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log("Javascript loaded")
 
 setInterval( ()=>{
   var today = new Date();
-	var time = today.getHours() + ":" + String(today.getMinutes()).padStart(2, '0') + "<span>:" + String(today.getSeconds()).padStart(2, '0') + "</span>"
+	var time = String(today.getHours()).padStart(2, '0') + ":" + String(today.getMinutes()).padStart(2, '0') + "<span>:" + String(today.getSeconds()).padStart(2, '0') + "</span>"
 	document.querySelector("p").innerHTML = time;
 }, 100)
